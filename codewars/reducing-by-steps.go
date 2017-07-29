f4576c5890f7498a2750a33bab6409cc1a58e2a2
Rafael Espinoza <nerddiffer@gmail.com>
2017-07-29 11:58:34 -0700

    Add solution to reduce-by-step
    Squashed commit of the following:
    
    commit b411bfda55af70cdfdfb4127f8846053c5847d4c
    Author: Rafael Espinoza <nerddiffer@gmail.com>
    Date:   Sat Jul 29 11:55:39 2017 -0700
    
        reduce-by-step: Solve problem
    
    commit 2b36f38971c4b34b997a12bb5959a378679072d7
    Author: Rafael Espinoza <nerddiffer@gmail.com>
    Date:   Sat Jul 29 11:23:09 2017 -0700
    
        reduce-by-step: Fix initial value
    
    commit 950a26f9424f5a3594473a0b0e73cdb74d3a8c3c
    Author: Rafael Espinoza <nerddiffer@gmail.com>
    Date:   Sat Jul 29 11:07:57 2017 -0700
    
        reduce-by-step: Start work on solution

---
+++ codewars/reducing-by-steps.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Abs(x int) int {
+	if x < 0 {
+		return x * -1
+	} else {
+		return x
+	}
+}
+
+func Gcdi(x, y int) int {
+	if y == 0 {
+		return x
+	}
+
+	remainder := x % y
+
+	if remainder == 0 {
+		return Abs(y) // base case
+	} else {
+		return Gcdi(y, remainder)
+	}
+}
+
+func Lcmu(x, y int) int {
+	a := Abs(x * y)
+	b := Gcdi(x, y)
+	return Abs(a / b)
+}
+
+func Som(x, y int) int {
+	return x + y
+}
+
+func Maxi(x, y int) int {
+	if x >= y {
+		return x
+	} else {
+		return y
+	}
+}
+
+func Mini(x, y int) int {
+	if x <= y {
+		return x
+	} else {
+		return y
+	}
+}
+
+type FParam func(int, int) int
+
+func OperArray(f FParam, arr []int, init int) []int {
+	length := len(arr)
+	result := make([]int, length)
+	prev := init
+
+	for i := 0; i < length; i++ {
+		val := f(prev, arr[i])
+		result[i] = val
+		prev = val
+	}
+
+	return result
+}
+
+func main() {
+	fmt.Println(Gcdi(48, 18))
+	fmt.Println(Lcmu(21, 6))
+
+	a := [6]int{18, 69, -90, -78, 65, 40}
+	s := a[:]
+
+	fmt.Println(OperArray(Gcdi, s, s[0])) // => [18, 3, 3, 3, 1, 1]
+	fmt.Println(OperArray(Lcmu, s, s[0])) // => [18, 414, 2070, 26910, 26910, 107640]
+	fmt.Println(OperArray(Som, s, 0))     // => [18, 87, -3, -81, -16, 24]
+	fmt.Println(OperArray(Mini, s, s[0])) // => [18, 18, -90, -90, -90, -90]
+	fmt.Println(OperArray(Maxi, s, s[0])) // => [18, 69, 69, 69, 69, 69]
+}
