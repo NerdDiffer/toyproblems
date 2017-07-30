81f09ddf7bb78f471ad5678e1df7269fcd0e83a5
Rafael Espinoza <nerddiffer@gmail.com>
2017-07-30 12:35:22 -0700

    Solve keyboard row
    Squashed commit of the following:
    
    commit d16bc244ce406e5200a5db6b3dbc394473bff3f6
    Author: Rafael Espinoza <nerddiffer@gmail.com>
    Date:   Sun Jul 30 12:01:55 2017 -0700
    
        keyboard-row: Clean up a little bit
    
    commit d2b815159ea479244da560ceb1b1764f9ebefebd
    Author: Rafael Espinoza <nerddiffer@gmail.com>
    Date:   Sun Jul 30 11:48:44 2017 -0700
    
        keyboard-row: Solve keyboard row problem

---
+++ leetcode/keyboard-row.go
@@ -0,0 +1,55 @@
+// https://leetcode.com/problems/keyboard-row
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	words := []string{"Hello", "Alaska", "Dad", "Peace", "TypeWriter"}
+	fmt.Println(findWords(words))
+}
+
+func findWords(words []string) []string {
+	// map of letter to row number
+	var keyToRow = map[string]int{
+		"q": 1, "w": 1, "e": 1, "r": 1, "t": 1, "y": 1, "u": 1, "i": 1, "o": 1, "p": 1,
+		"a": 2, "s": 2, "d": 2, "f": 2, "g": 2, "h": 2, "j": 2, "k": 2, "l": 2,
+		"z": 3, "x": 3, "c": 3, "v": 3, "b": 3, "n": 3, "m": 3,
+	}
+
+	getMapKey := func(word string, ind int) string {
+		return strings.ToLower(string(word[ind]))
+	}
+
+	result := []string{}
+
+	// for each word in the list of words:
+	for _, word := range words {
+		is_same := true
+
+		key := getMapKey(word, 0)
+		cmp := keyToRow[key]
+
+		// look at every letter in the word
+		for j := 1; j < len(word); j++ {
+			// are all of the (lower-case) letters in the same row?
+			key = getMapKey(word, j)
+			row := keyToRow[key]
+
+			// if no, move to next word in list of words
+			if row != cmp {
+				is_same = false
+				break
+			}
+		}
+
+		// if yes, add to result
+		if is_same {
+			result = append(result, word)
+		}
+	}
+
+	return result
+}
