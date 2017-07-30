c81e0c379b8809c8bb8293d6fe72429e863b67f9
Rafael Espinoza <nerddiffer@gmail.com>
2017-07-30 13:41:09 -0700

    Solve reverse string 2

--- leetcode/reverse-string.go
+++ leetcode/reverse-string.go
@@ -18,7 +18,52 @@ func reverseString(s string) string {
 	return strings.Join(slice, "")
 }
 
+// Given a string and an integer k, you need to reverse the first k characters for
+// every 2k characters counting from the start of the string. If there are less
+// than k characters left, reverse all of them. If there are less than 2k but
+// greater than or equal to k characters, then reverse the first k characters and
+// left the other as original.
+func reverseStr(s string, k int) string {
+	if k < 2 {
+		return s
+	}
+
+	length := len(s)
+	result := make([]string, 0, length)
+	shouldReverse := true
+
+	for i := 0; i < length; {
+		numRemaining := length - i
+		var strSlice string
+
+		if numRemaining < k {
+			strSlice = s[i:]
+		} else {
+			strSlice = s[i : i+k]
+		}
+
+		toAppend := strSlice
+
+		if shouldReverse {
+			toAppend = reverseString(strSlice)
+		}
+
+		result = append(result, toAppend)
+		shouldReverse = !shouldReverse
+		i += k
+	}
+
+	return strings.Join(result, "")
+}
+
 func main() {
-	fmt.Println(reverseString("racecar"))
-	fmt.Println(reverseString("foobar"))
+	//fmt.Println(reverseString("racecar"))
+	//fmt.Println(reverseString("foobar"))
+
+	fmt.Println(reverseStr("abcdefg", 2)) // bacdfeg
+	fmt.Println(reverseStr("abcdefg", 1)) // abcdefg
+	fmt.Println(reverseStr("abcd", 2))    // bacd
+	fmt.Println(reverseStr("abcdefg", 4)) // dbcaefg
+	// just don't crash ...
+	fmt.Println(reverseStr("krmyfshbspcgtesxnnljhfursyissjnsocgdhgfxubewllxzqhpasguvlrxtkgatzfybprfmmfithphckksnvjkcvnsqgsgosfxc", 20))
 }
