ae57dbb0f338f11f80595c9972cafb33bc52ce68
Rafael Espinoza <nerddiffer@gmail.com>
2017-08-12 11:53:19 -0700

    merge-two-lists: Fix to use LeetCode's Linked List

--- leetcode/merge-two-lists.go
+++ leetcode/merge-two-lists.go
@@ -1,51 +1,64 @@
-package main
+package leetcode
 
 import (
-	tph "github.com/NerdDiffer/toyproblems/helpers"
+	"fmt"
 )
 
-var joinedList = new(tph.SList)
-
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
-func mergeTwoLists(l1 *tph.ListNode, l2 *tph.ListNode) *tph.ListNode {
-	if l1 == nil && l2 == nil { // are both lists done?
-		return joinedList.Head
-	} else if l1 == nil && l2 != nil {
-		joinedList.Push(l2.Val)
-		return mergeTwoLists(l1, l2.Next())
-	} else if l1 != nil && l2 == nil {
-		joinedList.Push(l1.Val)
-		return mergeTwoLists(l1.Next(), l2)
-	} else {
-		// take the lower value
-		if l1.Val <= l2.Val {
-			joinedList.Push(l1.Val)
-			return mergeTwoLists(l1.Next(), l2)
-		} else {
-			joinedList.Push(l2.Val)
-			return mergeTwoLists(l1, l2.Next())
-		}
-	}
+type ListNode struct {
+	Val  int
+	Next *ListNode
 }
 
-func main() {
-	l1 := new(tph.SList)
-	l2 := new(tph.SList)
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	result := &ListNode{}
+	curr := result
 
-	l1.Push(10)
-	l1.Push(30)
-	l1.Push(50)
-	l2.Push(20)
-	l2.Push(40)
+	for l1 != nil || l2 != nil {
+		if l1 == nil {
+			curr.Next = l2
+			l2 = nil
+			continue
+		} else if l2 == nil {
+			curr.Next = l1
+			l1 = nil
+			continue
+		}
 
-	l1.Print()
-	l2.Print()
-	mergeTwoLists(l1.Head, l2.Head)
-	joinedList.Print()
+		if l1.Val <= l2.Val {
+			curr.Next = l1
+			curr, l1 = curr.Next, l1.Next
+		} else {
+			curr.Next = l2
+			curr, l2 = curr.Next, l2.Next
+		}
+	}
+
+	return result.Next
+}
+
+// for quick & dirty QA, change package name & this function name to `main`
+func run_mergeTwoLists() {
+	l1 := &ListNode{Val: 10}
+	l1.Next = &ListNode{Val: 30}
+	l1.Next.Next = &ListNode{Val: 50}
+
+	l2 := &ListNode{Val: 20}
+	l2.Next = &ListNode{Val: 40}
+
+	printListNode(l1)
+	printListNode(l2)
+
+	merged := mergeTwoLists(l1, l2)
+	printListNode(merged)
+}
+
+func printListNode(n *ListNode) {
+	curr := n
+
+	for curr != nil {
+		fmt.Printf("%+v -> ", curr.Val)
+		curr = curr.Next
+	}
+
+	fmt.Println()
 }
